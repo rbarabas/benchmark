c9fef53a6f206ecb2408e2857fca51da9063c533
Robert Barabas <robert@bolt.com>
2021-06-28 13:59:25 -0400

    Update processing script for better output

--- process.sh
+++ process.sh
@@ -3,42 +3,65 @@
 source $(dirname $0)/settings.sh
 source $(dirname $0)/common.sh
 
-function plot_threads() {
-  for T in ${THREADS}; do
-      THREAD_PATH="${TARGET_DIR}/threads_${T}"
-      THREAD_DATA="${THREAD_PATH}.data"
-      THREAD_TPS="${THREAD_PATH}.tps"
-      THREAD_QPS="${THREAD_PATH}.qps"
-      THREAD_RWO="${THREAD_PATH}.rwo"
-      THREAD_LAT="${THREAD_PATH}.lat"
-      THREAD_PLOT="${THREAD_PATH}.gnuplot"
-      TPS_PLOT="${THREAD_PATH}_tps.png"
-      QPS_PLOT="${THREAD_PATH}_qps.png"
-      RWO_PLOT="${THREAD_PATH}_rwo.png"
-      LAT_PLOT="${THREAD_PATH}_lat.png"
-      THREAD_PNG="${THREAD_PATH}.png"
-      if [ ! -f ${THREAD_DATA} ]; then
-          error 2 "missing data: ${THREAD_DATA}"
-          continue
+# Data mapping in format description:column (awk output column number)
+DATA_MAP="TPS:7 QPS:9 latency:14 reads:11:2 writes:11:3 other:11:4"
+
+function process() {
+  for d in ${DATA_MAP}; do
+    dtyp=$(echo ${d} | cut -d: -f1)
+    dcol=$(echo ${d} | cut -d: -f2)
+    scol=$(echo ${d} | cut -d: -f3)
+    for i in ${THREADS}; do
+      path="${TARGET_DIR}/threads_${i}"
+      dout="${path}.${dtyp}"
+      if [ -z "$scol" ]; then
+        cat "${path}.data"                                  \
+        | awk "{if (\$0 ~ /thds/){ print \$2, \$${dcol} }}" \
+        | tr -d ')s' | tr '/' ' ' > ${dout}
+      else
+        cat "${path}.data"                                  \
+        | awk "{if (\$0 ~ /thds/){ print \$2, \$${dcol} }}" \
+        | tr -d ')s' | tr '/' ' '                           \
+        | awk "{print \$1, \$${scol}}" > ${dout}
       fi
-      cat "${THREAD_DATA}" | awk '{if ($0 ~ /thds/){ print $2, $7 }}' | tr -d ')s' | tr '/' ' ' > ${THREAD_TPS}
-      cat "${THREAD_DATA}" | awk '{if ($0 ~ /thds/){ print $2, $9 }}' | tr -d ')s' | tr '/' ' ' > ${THREAD_QPS}
-      cat "${THREAD_DATA}" | awk '{if ($0 ~ /thds/){ print $2, $11 }}' | tr -d ')s' | tr '/' ' '> ${THREAD_RWO}
-      cat "${THREAD_DATA}" | awk '{if ($0 ~ /thds/){ print $2, $14 }}' | tr -d ')s' | tr '/' ' '> ${THREAD_LAT}
-      cat << PLOT > ${THREAD_PLOT}
+    done
+  done
+ }
+
+function plot() {
+  plotfile="${TARGET_DIR}/benchmark.gnuplot"
+  pngfile="${TARGET_DIR}/benchmark.png"
+  plotcmds=""
+  for d in ${DATA_MAP}; do
+    plotcmd=""
+    dtyp=$(echo ${d} | cut -d: -f1)
+    dcol=$(echo ${d} | cut -d: -f2)
+    for i in ${THREADS}; do
+      path="${TARGET_DIR}/threads_${i}"
+      datafile="${path}.${dtyp}"
+      TYPE_DATA="${TARGET_DIR}/threads_${i}.${dtyp}"
+      thiscmd="\"${datafile}\" using 1:2 title \"${i} thread\" with lines"
+      if [ -z "${plotcmd}" ]; then
+        plotcmd=$(echo -e "set ylabel \"${dtyp}\"\nplot ${thiscmd}")
+      else
+        plotcmd=$(echo -e "${plotcmd}, ${thiscmd}")
+      fi
+    done
+    plotcmds=$(echo -e "${plotcmds}\n${plotcmd}")
+  done
+
+  cat << PLOT > ${plotfile}
 set xdata time
 set timefmt "%s"
-set xlabel "Time"
-set ylabel "QPS"
-plot "${THREAD_TPS}" using 1:2 title "TPS" with lines,     \
-     "${THREAD_QPS}" using 1:2 title "QPS" with lines,     \
-     "${THREAD_RWO}" using 1:2 title "R/W/O" with lines,   \
-     "${THREAD_LAT}" using 1:2 title "Latency" with lines
-set terminal png font "/Library/Fonts/Arial Unicode.ttf" 14
-set output "${THREAD_PNG}"
+set term png size 1920,1080
+set output "${pngfile}"
+set multiplot layout 2,3 columnsfirst title "Database Benchmark Results"
+set format x '%s'
+${plotcmds}
+unset multiplot
 PLOT
-      gnuplot -persist ${THREAD_PLOT}
-  done
+
+  gnuplot -persist ${plotfile}
 }
 
 while getopts "d:" opt; do
@@ -49,5 +72,6 @@ done
 
 [ -z "${TARGET_DIR}" ] && error 1 "please specify data directory"
 
-plot_threads
+process
+plot
 
